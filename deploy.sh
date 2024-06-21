9ff3ed7b264924eccca57f3b3596f1c11d8b8385
Ted Romer <tromer@gmail.com>
2024-06-20 21:11:35 -0700

    move functions to us-west1

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 cd $(realpath "$(dirname "${BASH_SOURCE[0]}")") &&
     gcloud --project pennantchase-256 \
-	   functions deploy --gen2 --region=us-central1 --runtime=python312 \
+	   functions deploy --gen2 --region=us-west1 --runtime=python312 \
 	   process_box_score \
 	   --trigger-location=us-west1 --trigger-resource=pc256-box-scores \
 	   --trigger-event=google.storage.object.finalize
--- new-games-to-db/deploy.sh
+++ new-games-to-db/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash 
 cd $(realpath "$(dirname "${BASH_SOURCE[0]}")") &&
     gcloud --project pennantchase-256 \
-	   functions deploy --gen2 --region=us-central1 --runtime=python312 \
+	   functions deploy --gen2 --region=us-west1 --runtime=python312 \
+	   --no-allow-unauthenticated \
 	   new_games_to_db --trigger-http
