df00b2d5071f840529ff2d38646f926b78ff0ec0
Ted Romer <tromer@gmail.com>
2025-02-25 18:13:54 -0800

    fix a url, plus cosmetic changes

--- new-games-to-db/main.py
+++ new-games-to-db/main.py
@@ -19,7 +19,7 @@ from pprint import pprint
 LEAGUE_ID = '256'  # The Show
 BUCKET = 'pc256-box-scores'
 CONTENT_TYPE = 'text/html; charset=utf-8'
-PAST_STANDINGS_URL = 'https://www.pennantchase.com/lgPastStandings.aspx?lgId=%s' % LEAGUE_ID
+PAST_STANDINGS_URL = f'https://www.pennantchase.com/lgPastStandings.aspx?lgId={LEAGUE_ID}'
 
 @firestore.transactional
 def write_new_document(transaction, ref, data):
@@ -49,7 +49,7 @@ def new_games_to_db(request):
     mydb = db.collection(u'mydb')
 
   if not day:
-    r = requests.get('https://www.pennantchase.com/lgScoreboard.aspx?lgid=%s' % LEAGUE_ID)
+    r = requests.get(f'https://www.pennantchase.com/baseballleague/scoreboard?lgid={LEAGUE_ID}')
     soup = bs4.BeautifulSoup(r.content, 'html.parser')
     select = soup.find_all(lambda tag: tag.has_attr('id') and tag['id'] == 'wday')[0]
     day_elts = select.find_all(lambda tag: tag.has_attr('value'))
@@ -57,11 +57,11 @@ def new_games_to_db(request):
       day = max([int(e['value']) for e in day_elts])
     else:
       day = 0
-    print('Starting from day %d' % day, file=sys.stdout)
+    print(f'Starting from day {day}', file=sys.stdout)
 
   if not year:
     # TODO hope that if we're in playoffs we get the year right!
-    r = requests.get('https://www.pennantchase.com/lgSchedule.aspx?lgid=%s' % LEAGUE_ID)
+    r = requests.get(f'https://www.pennantchase.com/lgSchedule.aspx?lgid={LEAGUE_ID}')
     playoffs = r.content.decode().find('Playoff') >= 0
       
     r = requests.get(PAST_STANDINGS_URL)
@@ -71,7 +71,7 @@ def new_games_to_db(request):
       raise Exception("Couldn't determine year")
     last_year = int(last_year_str)
     year = last_year + (0 if playoffs else 1)
-  print('Year = %d' % year, file=sys.stdout)
+  print(f'{year=}', file=sys.stdout)
 
   fully_processed_count = 0
 
@@ -79,8 +79,8 @@ def new_games_to_db(request):
   considered = 0
   while day >= 1 and considered < limit:
     considered += 1
-    print('considering day %d' % day, file=sys.stdout)
-    day_url = 'https://www.pennantchase.com/lgScoreboard.aspx?lgid=256&scoreday=%d' % day
+    print(f'considering {day=}', file=sys.stdout)
+    day_url = f'https://www.pennantchase.com/baseballleague/scoreboard?lgid={LEAGUE_ID}&scoreday={day}'
     r = requests.get(day_url)
     r.raise_for_status()
     soup = bs4.BeautifulSoup(r.content, 'html.parser')
@@ -96,7 +96,7 @@ def new_games_to_db(request):
       rows = score_table.find_all('tr')
       header = [c.text for c in rows[0]]
       if header != ['Final', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'x', 'R', 'H', 'E']:
-        raise Exception('Bad header ' % header)
+        raise Exception(f'Bad header {header}')
       away_home_raw = rows[1:3]
       away_home_ids = []
       away_home_runs = []
@@ -135,15 +135,15 @@ def new_games_to_db(request):
         # Check if document is in db. This is here in case game_id turns out not to be unique.
         db_dict = ref.get().to_dict()
         if document != db_dict:
-          raise ValueError('%s: %s %s but db contains %s' % (game_id, ('Wrote' if wrote else 'Tried to write'), document, db_dict))
+          raise ValueError(f"{game_id}: {'Wrote' if wrote else 'Tried to write'} {document} but db contains {db_dict}")
       else:
-        print('Dry run, would have tried writing %s: %s' % (game_id, document))
+        print(f'Dry run, would have tried writing {game_id}: {document}')
       
     if score_count > 0 and upload_count == 0:
-      print('already processed day %d' % day, file=sys.stdout)
+      print(f'already processed {day=}', file=sys.stdout)
       fully_processed_count += 1
       if fully_processed_count > 1 and not keep_going:
-        print('already processed %d days, stopping' % fully_processed_count)
+        print(f'already processed {fully_processed_count} days, stopping')
         break
     
     day -= 1
