f80e736034eb2c387e5f0a0f5cec3571d645c83c
Ted Romer <tromer@gmail.com>
2025-05-24 09:09:27 -0700

    better though imperfect year logic

--- new-games-to-db/main.py
+++ new-games-to-db/main.py
@@ -25,12 +25,33 @@ PAST_STANDINGS_URL = f'https://www.pennantchase.com/lgPastStandings.aspx?lgId={L
 def write_new_document(transaction, ref, data):
   transaction.create(ref, data)
 
+
+def equal_except_year(a, b):
+  a2 = a.copy()
+  b2 = b.copy()
+  if 'year' in a2:
+    del a2['year']
+  if 'year' in b2:
+    del b2['year']
+  return a2 == b2
+
+
 def new_games_to_db(request):
   p = argparse.ArgumentParser()
   p.add_argument('-d', '--day', type=int, default=None, required=False)
   p.add_argument('-y', '--year', type=int, default=None, required=False)
-  p.add_argument('-l', '--limit', type=int, default=float('inf'), required=False)
-  p.add_argument('-k', '--keep_going', default=False, action='store_true')
+  p.add_argument(
+    '-l', '--limit', type=int, default=float('inf'), required=False,
+    help='Process at most limit days')
+  p.add_argument(
+    '-k', '--keep_going', default=False, action='store_true',
+    help='Keep looking beyond the latest two days that already have all games in db')
+  p.add_argument(
+    '-i', '--ignore_errors', default=False, action='store_true',
+    help='Accumulate errors regarding mismatched values instead of failing immediately')
+  p.add_argument(
+    '-f', '--force', default=False, action='store_true',
+    help='Overwrite mismatched values if the only difference is the year')
   p.add_argument('-n', '--dry_run', default=False, action='store_true')
   p.add_argument('--nodry_run', dest='dry_run', action='store_false')
   r = p.parse_args()
@@ -38,6 +59,8 @@ def new_games_to_db(request):
   year = r.year
   limit = r.limit
   keep_going = r.keep_going
+  ignore_errors = r.ignore_errors
+  force = r.force
   dry_run = r.dry_run
   if not dry_run:
     # Use the application default credentials
@@ -57,24 +80,25 @@ def new_games_to_db(request):
       day = max([int(e['value']) for e in day_elts])
     else:
       day = 0
-    print(f'Starting from day {day}', file=sys.stdout)
+      print(f'Starting from day {day}', file=sys.stdout)
 
   if not year:
-    # TODO hope that if we're in playoffs we get the year right!
-    r = requests.get(f'https://www.pennantchase.com/lgSchedule.aspx?lgid={LEAGUE_ID}')
-    playoffs = r.content.decode().find('Playoff') >= 0
-      
-    r = requests.get(PAST_STANDINGS_URL)
+    # TODO there is a race condition here depending on when we process
+    # the day with the last World Series game and when the World
+    # Series Champion gets updated.
+    r = requests.get(f'https://www.pennantchase.com/lgHistory.aspx?lgid={LEAGUE_ID}')
     r.raise_for_status()
-    last_year_str = re.sub(r"^.*Last Year's Standings: ([0-9]+)[^0-9].*$", r'\1', r.content.decode(), flags=re.DOTALL)
-    if not re.match(r'^[0-9]+$', last_year_str):
-      raise Exception("Couldn't determine year")
-    last_year = int(last_year_str)
-    year = last_year + (0 if playoffs else 1)
-  print(f'{year=}', file=sys.stdout)
+    soup = bs4.BeautifulSoup(r.content, 'html.parser')
+    last_wsc = soup.find('p')
+    last_year_str, rest = last_wsc.text.split(' ', 1)
+    if not rest.startswith('World Series Champion'):
+      raise Exception(f"Couldn't determine year from {last_wsc.text}")
+    year = int(last_year_str) + 1
+    print(f'{year=}', file=sys.stdout)
 
   fully_processed_count = 0
-
+  error_count = 0
+  
   # for each day
   considered = 0
   while day >= 1 and considered < limit:
@@ -89,7 +113,7 @@ def new_games_to_db(request):
     score_count = len(score_tables)
     if score_count == 0:
       time.sleep(5)
-    
+      
     upload_count = 0
     #  for each game
     for score_table in score_tables:
@@ -106,8 +130,8 @@ def new_games_to_db(request):
         team_id = re.match(r'.*tid=([^&]+)', line_elts[0].find_all('a')[0]['href'])[1]
         away_home_ids.append(team_id)
         away_home_runs.append(int(line_elts[11].text))
-      box_score_url = 'https://www.pennantchase.com/' + rows[-1].find_all('a')[0]['href']
-      game_id = re.match(r'.*sid=([^&]+)', box_score_url)[1]
+        box_score_url = 'https://www.pennantchase.com/' + rows[-1].find_all('a')[0]['href']
+        game_id = re.match(r'.*sid=([^&]+)', box_score_url)[1]
 
       # and since we have them handy
       # home_runs
@@ -132,21 +156,36 @@ def new_games_to_db(request):
           print('wrote', game_id)
         except exceptions.AlreadyExists as e:
           print(game_id, 'already exists')
-        # Check if document is in db. This is here in case game_id turns out not to be unique.
+          # Check if document is in db. This is here in case game_id turns out not to be unique.
         db_dict = ref.get().to_dict()
         if document != db_dict:
-          raise ValueError(f"{game_id}: {'Wrote' if wrote else 'Tried to write'} {document} but db contains {db_dict}")
+          # TODO would be nice to do this stuff transactionally
+          if force and equal_except_year(document, db_dict):
+            print(f"Overwriting year={document['year']} in {db_dict}")
+            ref.set(document)
+          else:
+            error_count += 1
+            # The "Wrote" case should be impossible
+            message = f"{game_id}: {'Wrote' if wrote else 'Tried to write'} {document} but db contains {db_dict}"
+            if ignore_errors:
+              print(message)
+            else:
+              raise ValueError(message)
       else:
         print(f'Dry run, would have tried writing {game_id}: {document}')
-      
+        
     if score_count > 0 and upload_count == 0:
       print(f'already processed {day=}', file=sys.stdout)
       fully_processed_count += 1
       if fully_processed_count > 1 and not keep_going:
         print(f'already processed {fully_processed_count} days, stopping')
         break
-    
+      
     day -= 1
+
+  if error_count > 0:
+    raise Exception(f'{error_count} games with database mismatches')
+  
   return ''
 
 
