e27f4bcc94aac072cd467b4faedc6e89d6b33920
Ted Romer <tromer@gmail.com>
2025-06-07 23:35:17 -0700

    docker

---
+++ ensure_trigger.sh
@@ -0,0 +1,22 @@
+# TODO
+#!/bin/bash
+
+set -o pipefail
+
+shopt -s expand_aliases
+source $HOME/.bash_script_aliases
+
+PROJECT=pennantchase-256
+LOCATION=us-west1
+SERVICE=process-box-score
+SA=eventarc-trigger@${PROJECT}.iam.gserviceaccount.com
+
+ensure_eventarc_trigger_account $PROJECT ${SERVICE} $SA &&
+    (gcloud --project=${PROJECT} eventarc triggers describe --location=$LOCATION boxscore >& /dev/null ||
+	 gcloud --project=${PROJECT} eventarc triggers create \
+		--location=${LOCATION} \
+		--event-filters="type=google.cloud.storage.object.v1.finalized" \
+		--event-filters="bucket=pc256-box-scores" \
+		--destination-run-service=${SERVICE} \
+		--service-account=$SA \
+		boxscore)
---
+++ start.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+port = os.environ.get('PORT', '8080')
+cmd = [
+    'gunicorn', 
+    f'--bind=:{port}', 
+    '--workers=1', 
+    '--threads=8', 
+    '--timeout=0',
+    # '--log-level=debug',
+    'main:app'
+]
+print(f'running gunicorn args={cmd}', file=sys.stderr)
+os.execvp('gunicorn', cmd)
