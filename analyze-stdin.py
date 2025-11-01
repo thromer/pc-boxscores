6352afbe833f23b5533acf0a0b52f5f2f72cdbe2
Ted Romer <tromer@gmail.com>
2025-10-31 19:30:28 -0700

    get_blob instead of Bucket.blob and other cleanup

--- analyze-stdin.py
+++ analyze-stdin.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
 
-import os
 import sys
 
 from lib import analyze
 from lib import pcweb
 
-if (
-    "GOOGLE_APPLICATION_CREDENTIALS" not in os.environ
-    and "FUNCTION_TARGET" not in os.environ
-    and "FUNCTION_TRIGGER_TYPE" not in os.environ
-    and ("CLOUD_SHELL" not in os.environ or not os.environ["CLOUD_SHELL"])
-):
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "./service-account-key.json"
+# if (
+#     "GOOGLE_APPLICATION_CREDENTIALS" not in os.environ
+#     and "FUNCTION_TARGET" not in os.environ
+#     and "FUNCTION_TRIGGER_TYPE" not in os.environ
+#     and ("CLOUD_SHELL" not in os.environ or not os.environ["CLOUD_SHELL"])
+# ):
+#     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "./service-account-key.json"
 
 
 def main():
